da891add2044084097022818501f7ed54ee499a4
Bogdan Kozhukhov <b.kozhukhov@toolstrek.ru>
2022-04-07 19:06:12 +0300

    Получение изображения из галереии и отображение его в приложении

--- app/src/main/java/com/project/safedatastorage/MainActivity.java
+++ app/src/main/java/com/project/safedatastorage/MainActivity.java
@@ -1,14 +1,52 @@
 package com.project.safedatastorage;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+
+    ImageView imageView;
+    Button button;
+    Uri imageUri;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        imageView = findViewById(R.id.imageView);
+        button = findViewById(R.id.buttonLoadPicture);
+
+        // Вместо переопределения метода onActivityResult используется это выражение
+        // https://stackoverflow.com/questions/62671106/onactivityresult-method-is-deprecated-what-is-the-alternative
+        ActivityResultLauncher<Intent> someActivityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        imageUri = result.getData().getData();
+                        imageView.setImageURI(imageUri);
+                    }
+                });
+
+        button.setOnClickListener((View v) -> {
+                Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+                someActivityResultLauncher.launch(gallery);
+        });
     }
 }
